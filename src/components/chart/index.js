7f2cdbe11191ee815b0f753b5f059d658c94415c
Hrushikesh Mohanty <hrushikeshmohanty90@gmail.com>
2021-12-04 20:29:43 +0530

    previous chart is deleted and new chart is redrawn

--- src/components/chart/index.js
+++ src/components/chart/index.js
@@ -33,7 +33,7 @@ export default function Chart() {
                   return {
                     area_name: e.area_name,
                     value: e.chartdata.filter((obj) => obj.name === goal)[0]
-                      .value,
+                      .value
                   };
                 })
               )
@@ -47,80 +47,84 @@ export default function Chart() {
 
   const drawChart = (data) => {
     
+    d3.select(d3Chart.current).selectAll("svg").remove();
+
     const margin = { top: 50, right: 20, bottom: 30, left: 40 };
 
-      const chartWidth =
-        parseInt(d3.select("#chart").style("width")) -
-        margin.left -
-        margin.right;
+    const chartWidth =
+      parseInt(d3.select("#chart").style("width")) - margin.left - margin.right;
 
+    const chartHeight =
+      parseInt(d3.select("#chart").style("height")) -
+      margin.top -
+      margin.bottom;
 
-      const chartHeight =
-        parseInt(d3.select("#chart").style("height")) -
-        margin.top -
-        margin.bottom;
+    const svg = d3
+      .select(d3Chart.current)
+      .attr("id", "drawnChart")
+      .append("svg")
+      .attr("width", chartWidth + margin.left + margin.right)
+      .attr("height", chartHeight + margin.top + margin.bottom)
+      .style("border", "1px solid black")
+      .style("overflow", "visible");
 
-      const svg = d3
-        .select(d3Chart.current)
-        .attr("id", "drawnChart")
-        .append("svg")
-        .attr("width", chartWidth + margin.left + margin.right)
-        .attr("height", chartHeight + margin.top + margin.bottom)
-        .style('border', '1px solid black')
-        .style("overflow", "visible");
+    var selection = svg.selectAll("rect").data(data);
 
-      var selection = svg.selectAll('rect').data(data)
+    //scaling for svg
+    const xScale = d3
+      .scaleBand()
+      .domain(d3.range(data.length))
+      .range([margin.left, chartWidth - margin.right])
+      .padding(0.3);
 
-      //scaling for svg
-      const xScale = d3
-        .scaleBand()
-        .domain(d3.range(data.length))
-        .range([margin.left, chartWidth - margin.right])
-        .padding(0.3);
-        
-        const maxValue = d3.max(data, function (v) {
-          return v.value;
-        });
-  
-        const yScale = d3
-          .scaleLinear()
-          .domain([0, maxValue])
-          .range([chartHeight, margin.top]);
+    const maxValue = d3.max(data, function (v) {
+      return v.value;
+    });
 
-      selection.enter()
-        .append("g")
-        .attr("transform", "translate(0," + chartHeight + ")")
-        .call(
-          d3
-            .axisBottom(xScale)
-            .tickFormat((i) => data[i].area_name)
-            .ticks(5)
-            .tickPadding(2)
-            .tickSizeOuter(2)
-        ).selectAll('text').style("text-anchor", "end")
-        .attr("dx", "-.8em")
-        .attr("dy", ".15em").attr("transform", "rotate(-65)")
+    const yScale = d3
+      .scaleLinear()
+      .domain([0, maxValue])
+      .range([chartHeight, margin.top]);
 
-      
-      selection.enter()
-        .append("g")
-        .attr("transform", "translate(" + margin.left + ", 0)")
-        .call(d3.axisLeft(yScale));
+    selection
+      .enter()
+      .append("g")
+      .attr("transform", "translate(0," + chartHeight + ")")
+      .call(
+        d3
+          .axisBottom(xScale)
+          .tickFormat((i) => data[i].area_name)
+          .ticks(5)
+          .tickPadding(2)
+          .tickSizeOuter(2)
+      )
+      .selectAll("text")
+      .style("text-anchor", "end")
+      .attr("dx", "-.8em")
+      .attr("dy", ".15em")
+      .attr("transform", "rotate(-65)");
 
-      selection.enter()
-        .append("g")
-        .attr("fill", "royalblue")
-        .selectAll("rect")
-        .data(data)
-        .join("rect")
-        .transition().duration(300)
-        .attr("x", (d, i) => xScale(i))
-        .attr("y", (d) => yScale(d.value))
-        .attr("height", (d) => yScale(0) - yScale(d.value))
-        .attr("width", xScale.bandwidth());
+    selection
+      .enter()
+      .append("g")
+      .attr("transform", "translate(" + margin.left + ", 0)")
+      .call(d3.axisLeft(yScale));
 
-      
-      selection.exit().remove()
+    selection
+      .enter()
+      .append("g")
+      .attr("fill", "royalblue")
+      .selectAll("rect")
+      .data(data)
+      .join("rect")
+      .transition()
+      .duration(300)
+      .attr("x", (d, i) => xScale(i))
+      .attr("y", (d) => yScale(d.value))
+      .attr("height", (d) => yScale(0) - yScale(d.value))
+      .attr("width", xScale.bandwidth());
+
+    selection.exit().remove();
   }
 
   useEffect(() => {
@@ -128,10 +132,6 @@ export default function Chart() {
       drawChart(chartDataValues)
       
     }
-    return (()=>{
-      d3.select("#drawnChart").remove()
-      console.log('re-render')
-    })
   }, [chartDataValues]);
 
   
